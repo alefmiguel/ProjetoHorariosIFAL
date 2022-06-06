1b947eb2dfc63f49de4aa4c1850300c7ae12a51d
alefmiguel <alefmiguel45@gmail.com>
2022-06-06 13:29:28 -0300

    validação com ENTER

--- others/validacao.js
+++ others/validacao.js
@@ -1,15 +1,28 @@
-let botao = document.querySelector(".botão")
+let botao = document.querySelector(".botão");
+var usuario = document.querySelector(".nome");
+var senha = document.querySelector(".senha");
 
-botao.addEventListener("click", function(){
+function validaFormulario(){
     var usuario = document.querySelector(".nome")
     var senha = document.querySelector(".senha")
 
     if((usuario.value == "Alef" && senha.value == 12345678) || (usuario.value == "Mayllon" && senha.value == 12345678)){
         // se os programadores logarem no perfil
         alert("Entrei no perfil.")
+        location.replace('../index.html')
 		
     }
     else {
         alert("Você não entrou no perfil.")
     }
-})
+}
+
+botao.addEventListener("click", validaFormulario)
+
+document.addEventListener("keypress", (event) => {
+    if (event.key === 'Enter') {
+        if (usuario.value != '' && senha.value != '') {
+            validaFormulario()
+        }
+    }
+});
